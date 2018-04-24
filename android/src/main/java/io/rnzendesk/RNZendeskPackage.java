4f86a70f7dbe0a24a06aa72c31e296490b252772
David Chavez <david@dcvz.io>
2018-04-24 09:59:24 +0200

    Add Android bridge

---
+++ android/src/main/java/io/rnzendesk/RNZendeskPackage.java
@@ -0,0 +1,34 @@
+package io.dcvz.rnzendesk;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class RNZendeskPackage implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(
+        ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new RNZendeskBridge(reactContext));
+        return modules;
+    }
+
+    // This "Override" method is being kept for backwards compatibilty with React Native
+    // releases earlier than https://github.com/facebook/react-native/releases/tag/v0.47.0
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Arrays.<ViewManager>asList();
+    }
+}
