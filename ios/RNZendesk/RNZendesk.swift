911a213bf478624e5c2102615f6a62ff0605694a
David Chavez <david@dcvz.io>
2018-05-10 07:23:00 +0200

    [ios] Add early return to `identifyJWT`

--- ios/RNZendesk/RNZendesk.swift
+++ ios/RNZendesk/RNZendesk.swift
@@ -44,6 +44,7 @@ class RNZendesk: RCTEventEmitter {
     
     @objc(identifyJWT:)
     func identifyJWT(token: String) {
+        guard let token = token else { return }
         let identity = Identity.createJwt(token: token)
         Zendesk.instance?.setIdentity(identity)
     }
